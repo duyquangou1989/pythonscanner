8cc3f61b66c4ddaa2647ce33f790f2bac34dfeda
duyquangou1989 <duyquangou1989@gmail.com>
2020-03-21 22:24:52 +0700

    Update func

--- scanner.py
+++ scanner.py
@@ -2,21 +2,21 @@
 ###Scanner Python QuangTD
 from socket import *
 
+def scan(ip,start,end):
+	ports_open = []
+	for i in range(start,end):
+		s = socket(AF_INET,SOCK_STREAM)
+		s.settimeout(1)
+		result = s.connect_ex((ip,i))
+		print('Scanning Port: %d' % i)
+		if result == 0:
+			print('Gottcha --> %d' % i)
+			ports_open.append(i)
+		s.close()
+	return ports_open
+
 if __name__ == '__main__':
 	target = input('IP: ')
 	targetIP = gethostbyname(target)
 
-	arr_open = []
-
-	for i in range (20,2025):
-		s = socket(AF_INET, SOCK_STREAM)
-		s.settimeout(2)
-		result = s.connect_ex((targetIP,i))
-		print('Scanning Port : %d' % i )
-		if result == 0 :
-			arr_open.append(i)
-			#print('--> Port %d Open' % i)
-		#else:
-		#	print('--> Port %d Closed' % i)
-		s.close()
-	print(*arr_open)
+	print(scan(targetIP,80,445))
